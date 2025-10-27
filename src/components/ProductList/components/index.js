b4de243babcfba7e093a83968db024f1fbaefc5e
kkhs2 <shumkhk@gmail.com>
2025-10-27 20:18:51 +0000

    remove /src and /dist

--- src/components/ProductList/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './ProductListButton';
-export * from './ProductListCard';
-export * from './ProductListCollection';
-export * from './OrderListButton';
-export * from './OrderListTable';
-export * from './OrderListMobileCards';
--- src/components/QuoteCard/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './QuoteStacked';
-export * from './QuoteGraphic';
-export * from './QuoteArticle';
-export * from './QuoteSimple';
-export * from './QuoteCarousel';
